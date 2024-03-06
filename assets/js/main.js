960e43508002dd0d5cdcc0a6c914a0da6ffd4d66
giovannimedici <giovannigilsu@gmail.com>
2024-03-06 19:31:32 -0300

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,3 +125,13 @@ themeButton.addEventListener('click', () => {
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, {delay: 700})
+sr.reveal(`.home__social, .home___scroll`, {delay: 900, origin: 'bottom'})
